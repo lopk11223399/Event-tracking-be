fe9db0fdfdc5e2d970fdba7566f76ea6acdc859f
HieuNguyen <nguyenhieu11032002@gmail.com>
2023-09-17 15:58:11 +0700

    install helmet and limit

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 require("dotenv").config();
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import initRoutes from "./src/routes";
 require("./connect_DB");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json({ limit: "10mb" }));
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
+app.use(helmet());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 initRoutes(app);
